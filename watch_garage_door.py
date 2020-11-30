a3c15c1c790eb67ccc212847616eb1c1885cfea7
Alan Hamlett <alan.hamlett@gmail.com>
2020-11-30 07:31:26 -0800

    increase clock drift buffer to 2 minutes

--- watch_garage_door.py
+++ watch_garage_door.py
@@ -80,7 +80,7 @@ def get_token(fresh: bool = False) -> str:
             with open(SECRET_FILE) as fh:
                 token = fh.readline().strip()
                 expires_at = fh.readline().strip()
-                if token and int(expires_at or 0) > int(time.time() + 5):
+                if token and int(expires_at or 0) > int(time.time() + 120):
                     return token
         except:
             pass
